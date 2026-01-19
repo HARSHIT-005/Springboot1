e83cf2cb3e9ee7d7cc5bff455848f9de8af13be4
Harshit Chauhan <harshitchauhan988@gmail.com>
2026-01-19 15:25:08 +0530

    array list for 5 students

--- Day1/src/main/java/com/example/Day1/controller/home.java
+++ Day1/src/main/java/com/example/Day1/controller/home.java
@@ -1,6 +1,7 @@
 package com.example.Day1.controller;
 
-
+import java.util.ArrayList;
+import java.util.List;
 import com.example.Day1.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class home {
     @GetMapping("/")
-    public StudentModel getStudent(){
-        StudentModel student=new StudentModel(1,"hc","hc@gmail.com");
-        return student;
+    public List<StudentModel> getStudent(){
+        List<StudentModel> students=new ArrayList<>();
+        students.add(new StudentModel(1,"hc","hc1@gmail.com"));
+        students.add(new StudentModel(2,"hc2","hc2@gmail.com"));
+        students.add(new StudentModel(3,"hc3","hc3@gmail.com"));
+        students.add(new StudentModel(4,"hc4","hc4@gmail.com"));
+        students.add(new StudentModel(5,"hc5","hc5@gmail.com"));
+        return students;
     }
 
 }
